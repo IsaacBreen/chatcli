f98649201844f92fa6b80d5b69c1b84fefa385c4
IsaacBreen <mail@isaacbreen.com>
2023-03-02 16:28:47 +0900

    Added a new class, ChatGenerator, to handle the chat functionality.

--- chatcli.py
+++ chatcli.py
@@ -11,7 +11,57 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.key_binding import KeyPressEvent
 from prompt_toolkit.shortcuts import prompt
 
-T = TypeVar("T", Mapping, str)
+T = TypeVar("T", MutableMapping, str)
+
+
+class ChatGenerator:
+    def __init__(
+        self,
+        messages: Optional[List[Dict[str, str]]] = None,
+        *,
+        text_io: Optional[io.TextIOBase] = sys.stdout,
+        sep: Optional[str] = "\n",
+    ) -> None:
+        self.messages = messages or [{"role": "system", "content": "You are a helpful assistant."}]
+        self.text_io = text_io or sys.stdout
+        self.sep = sep
+
+    def __call__(self, user_message: str) -> MutableMapping:
+        assert isinstance(user_message, str)
+
+        # Add the user's message to the list of messages
+        self.messages.append({"role": "user", "content": user_message})
+
+        # Send the messages to the API, accumulate the responses, and print them as they come in
+        response_accumulated = None
+
+        for response in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages,
+            stream=True,
+        ):
+            if response_accumulated is None:
+                # Set the initial response to the first response (less the delta)
+                response_accumulated = response.choices[0]
+            else:
+                # Apply the delta to the accumulated response
+                response_accumulated.delta = string_tree_apply_delta(
+                    response_accumulated.delta, response.choices[0].delta
+                )
+            if self.text_io is not None and "content" in response.choices[0].delta:
+                self.text_io.write(response.choices[0].delta.content)
+                self.text_io.flush()
+
+        # Apply the separator
+        if self.text_io is not None:
+            self.text_io.write(self.sep)
+            self.text_io.flush()
+
+        # Rename the delta to message
+        response_accumulated.message = response_accumulated.pop("delta")
+
+        # Get the next message from the user
+        return response_accumulated
 
 
 def string_tree_apply_delta(tree: T, delta: T) -> T:
@@ -30,8 +80,8 @@ def string_tree_apply_delta(tree: T, delta: T) -> T:
     T
         The Python tree with the delta applied.
     """
-    if isinstance(tree, Mapping):
-        assert isinstance(delta, Mapping)
+    if isinstance(tree, MutableMapping):
+        assert isinstance(delta, MutableMapping)
         for key, value in delta.items():
             if key not in tree:
                 tree[key] = value
@@ -43,73 +93,6 @@ def string_tree_apply_delta(tree: T, delta: T) -> T:
         return tree + delta
     else:
         raise TypeError(f"Invalid type {type(tree)}")
-
-
-def chatgen(
-    messages: Optional[List[Dict[str, str]]] = None,
-    *,
-    text_io: Optional[io.TextIOBase] = sys.stdout,
-    sep: Optional[str] = "\n",
-) -> Generator[Mapping, str, None]:
-    """
-    Chat with an OpenAI API model using a Python generator. Send your messages one at a time. The response is printed to the console.
-
-    Parameters
-    ----------
-    messages : Optional[List[Dict[str, str]]], optional
-        A list of messages to send to the model. Each message should be a dictionary with a "role" key and a "content" key. You should call `next` or `.send(None)` prior to sending your first message to prime the generator.
-    text_io : Optional[io.TextIOBase], optional
-        The text IO to write the model's response to, by default sys.stdout
-    sep : Optional[str], optional
-        The separator to write between messages, by default "\\n"
-
-    Yields
-    ------
-    Mapping
-        The model's response: a Mapping provided by the OpenAI API.
-    """
-    if messages is None:
-        messages = [{"role": "system", "content": "You are a helpful assistant."}]
-
-    # Prime the generator
-    user_message = yield
-
-    while True:
-        assert isinstance(user_message, str)
-
-        # Add the user's message to the list of messages
-        messages.append({"role": "user", "content": user_message})
-
-        # Send the messages to the API, accumulate the responses, and print them as they come in
-        response_accumulated = None
-
-        for response in openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        ):
-            if response_accumulated is None:
-                # Set the initial response to the first response (less the delta)
-                response_accumulated = response.choices[0]
-            else:
-                # Apply the delta to the accumulated response
-                response_accumulated.delta = string_tree_apply_delta(
-                    response_accumulated.delta, response.choices[0].delta
-                )
-            if text_io is not None and "content" in response.choices[0].delta:
-                text_io.write(response.choices[0].delta.content)
-                text_io.flush()
-
-        # Apply the separator
-        if text_io is not None:
-            text_io.write(sep)
-            text_io.flush()
-
-        # Rename the delta to message
-        response_accumulated.message = response_accumulated.pop("delta")
-
-        # Get the next message from the user
-        user_message = yield response_accumulated
 
 
 def multiline_prompt(*, swap_newline_keys: bool) -> str:
@@ -131,7 +114,24 @@ def multiline_prompt(*, swap_newline_keys: bool) -> str:
         kb.add("enter")(submit)
 
     # Define a prompt continuation function
-    def prompt_continuation(width, line_number, wrap_count):
+    def prompt_continuation(width: int, line_number: int, wrap_count: int) -> str:
+        """
+        Return the continuation prompt.
+
+        Parameters
+        ----------
+        width : int
+            The width of the prompt.
+        line_number : int
+            The line number of the prompt.
+        wrap_count : int
+            The number of times the prompt has wrapped.
+
+        Returns
+        -------
+        str
+            The continuation prompt.
+        """
         return "... ".rjust(width)
 
     return prompt(
@@ -154,6 +154,8 @@ def chatcli(
         The system message to send to the model.
     assistant : Optional[str]
         The assistant message to send to the model.
+    swap_newline_keys : bool
+        Whether to swap the keys for submitting and entering a newline.
     """
     # Print a header
     chatcli_version = pkg_resources.get_distribution("chatcli").version
@@ -168,15 +170,14 @@ def chatcli(
         messages.append({"role": "assistant", "content": assistant})
 
     # Create the generator
-    chat = chatgen(messages)
-    next(chat)
+    chat = ChatGenerator(messages)
 
     # Get the first message from the user
     user_message = multiline_prompt(swap_newline_keys=swap_newline_keys)
 
     # Send the user's message to the generator
     while user_message != "exit":
-        chat.send(user_message)
+        chat(user_message)
         user_message = multiline_prompt(swap_newline_keys=swap_newline_keys)
 
 
@@ -188,15 +189,14 @@ def test_chatgen() -> None:
     messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
     # Create the generator
-    chat = chatgen(messages)
-    next(chat)
+    chat = ChatGenerator(messages)
 
     # Send the user's message to the generator
     assert (
         "Dodgers"
-        in chat.send("Who won the world series in 2020?")["message"]["content"]
+        in chat("Who won the world series in 2020?")["message"]["content"]
     )
-    assert "Texas" in chat.send("Where was it played?")["message"]["content"]
+    assert "Texas" in chat("Where was it played?")["message"]["content"]
 
 
 if __name__ == "__main__":
